9b63d194579eeaab3179adc8bfbd9cbb257e4abf
freetimezp <kraftukrnet@ukr.net>
2021-04-26 22:12:39 +0300

    php framework learning l52

--- core/admin/controller/BaseAdmin.php
+++ core/admin/controller/BaseAdmin.php
@@ -22,6 +22,7 @@ abstract class BaseAdmin extends BaseController
     protected $menu;
     protected $title;
 
+    protected $alias;
     protected $fileArray;
 
     protected $messages;
@@ -335,8 +336,20 @@ abstract class BaseAdmin extends BaseController
         }
     }
 
-    protected function checkExceptFields() {
+    protected function checkExceptFields($arr = []) {
+        if(!$arr) { $arr = $_POST; }
 
+        $except = [];
+
+        if(!$arr) {
+            foreach ($arr as $key => $item) {
+                if(!$this->columns[$key]) {
+                    $except[] = $key;
+                }
+            }
+        }
+
+        return $except;
     }
 
     protected function createFile() {
@@ -344,7 +357,51 @@ abstract class BaseAdmin extends BaseController
     }
 
     protected function createAlias($id = false) {
+        if($this->columns['alias']) {
+            if(!$_POST['alias']) {
+                if($_POST['name']) {
+                    $alias_str = $this->clearStr($_POST['name']);
+                }else{
+                    foreach ($_POST as $key => $item) {
+                        if(strpos($key, 'name') !== false && $item) {
+                            $alias_str = $this->clearStr($item);
+                            break;
+                        }
+                    }
+                }
+            }else{
+                $alias_str = $_POST['alias'] = $this->clearStr($_POST['alias']);
+            }
 
+            $textModify = new \libraries\TextModify();
+            $alias = $textModify->translit($alias_str);
+
+            $where['alias'] = $alias;
+            $operand[] = '=';
+
+            if($id) {
+                $where[$this->columns['id_row']] = $id;
+                $operand[] = '<>';
+            }
+
+            $res_alias = $this->model->get($this->table, [
+                'fields' => ['alias'],
+                'where' => $where,
+                'operand' => $operand,
+                'limit' => '1'
+            ])[0];
+
+            if(!$res_alias) {
+                $_POST['alias'] = $alias;
+            }else{
+                $this->alias = $alias;
+                $_POST['alias'] = '';
+            }
+
+            if($_POST['alias'] && $id) {
+                method_exists($this, 'checkOldAlias') && $this->checkOldAlias($id);
+            }
+        }
     }
 
     protected function updateMenuPosition() {
@@ -352,7 +409,20 @@ abstract class BaseAdmin extends BaseController
     }
 
     protected function checkAlias($id) {
+        if($id) {
+            if($this->alias) {
+                $this->alias .= '-' . $id;
 
+                $this->model->edit($this->table, [
+                    'fields' => ['alias' => $this->alias],
+                    'where' => [$this->columns['id_row'] => 'id']
+                ]);
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 

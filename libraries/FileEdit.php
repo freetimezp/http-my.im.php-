98ba34ebc4089a82aece1bc9308e9f166031926f
freetimezp <kraftukrnet@ukr.net>
2021-04-30 20:47:37 +0300

    php framework learning l56

--- libraries/FileEdit.php
+++ libraries/FileEdit.php
@@ -8,7 +8,7 @@ class FileEdit
     protected $directory;
 
     public function addFile($directory = false) {
-        if($directory) {
+        if(!$directory) {
             $this->directory = $_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR;
         }else{
             $this->directory = $directory;
@@ -49,10 +49,70 @@ class FileEdit
     }
 
     protected function createFile($file) {
-        
+        $fileNameArr = explode('.', $file['name']);
+        $ext = $fileNameArr[count($fileNameArr) - 1];
+        unset($fileNameArr[count($fileNameArr) - 1]);
+
+        $fileName = implode('.', $fileNameArr);
+
+        $fileName = (new TextModify())->translit($fileName);
+
+        $fileName = $this->checkFile($fileName, $ext);
+
+        $fileFullName = $this->directory . $fileName;
+
+        if($this->uploadFile($file['tmp_name'], $fileFullName)) {
+            return $fileName;
+        }
+
+        return false;
+    }
+
+    protected function uploadFile($tmpName, $dest) {
+        if(move_uploaded_file($tmpName, $dest)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function checkFile($fileName, $ext, $fileLastName = '') {
+        if(!file_exists($this->directory . $fileName . $fileLastName . '.' . $ext)) {
+            return $fileName . $fileLastName . '.' . $ext;
+        }
+
+        return $this->checkFile($fileName, $ext, '_' . hash('crc32', time() . mt_rand(1, 1000)));
     }
 
     public function getFiles() {
         return $this->imgArr;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

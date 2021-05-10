ff0e528059ed471965a1624878ccd7e9b6136804
freetimezp <kraftukrnet@ukr.net>
2021-05-10 19:50:29 +0300

    php framework learning l69

---
+++ core/admin/view/js/scripts.js
@@ -0,0 +1,11 @@
+document.querySelector('.sitemap-button').onclick = (e) => {
+    e.preventDefault();
+    Ajax({type: 'POST'})
+        .then((res) => {
+            console.log('success - ' + res);
+        })
+        .catch((res) => {
+            console.log('error - ' + res);
+        });
+}
+

8ff9f046df9f6a6880354205582eb90cc8e3bb61
freetimezp <kraftukrnet@ukr.net>
2021-05-17 22:14:23 +0300

    php framework learning l73

--- core/base/model/Crypt.php
+++ core/base/model/Crypt.php
@@ -18,7 +18,13 @@ class Crypt
         $cipherText = openssl_encrypt($str, $this->cryptMethod, CRYPT_KEY, OPENSSL_RAW_DATA, $iv);
         $hmac = hash_hmac($this->hashAlgoritm, $cipherText, CRYPT_KEY, true);
 
-        return base64_encode($iv . $hmac . $cipherText);
+        //return base64_encode($iv . $hmac . $cipherText);
+
+        $cipherText_comb = '112233445566778899';
+        $iv_comb = 'abcdefg';
+        $hmac_comb = '000000000000';
+
+        $res = $this->cryptCombine($cipherText_comb, $iv_comb, $hmac_comb);
     }
 
     public function decrypt($str) {
@@ -36,5 +42,39 @@ class Crypt
         }
 
         return false;
+    }
+
+    protected function cryptCombine($str, $iv, $hmac) {
+        $new_str = '';
+        $str_len = strlen($str);
+        $counter = (int)ceil(strlen(CRYPT_KEY) / ($str_len + strlen($hmac)));
+        $progress = 1;
+
+        if($counter >= $str_len) {
+            $counter = 1;
+        }
+
+        for($i = 0; $i < $str_len; $i++) {
+            if($counter < $str_len) {
+                if($counter === $i) {
+                    $new_str .= substr($iv, $progress - 1, 1);
+                    $progress++;
+                    $counter += $progress;
+                }
+            }else{
+                break;
+            }
+
+            $new_str .= substr($str, $i, 1);
+        }
+
+        $new_str .= substr($str, $i);
+        $new_str .= substr($iv, $progress - 1);
+
+        $new_str_half = (int)ceil(strlen($new_str) / 2);
+
+        $new_str = substr($new_str, 0, $new_str_half) . $hmac . substr($new_str, $new_str_half);
+
+        return base64_encode($new_str);
     }
 }
